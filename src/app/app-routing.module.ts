0015f9f980191488c6bfc049ec8d85b7cc7975f6
alej1286 <alej1286@gmail.com>
2021-12-20 14:11:23 -0500

    Init responsive sideNav Base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { PostsComponent } from './admin/dashboard/posts/posts.component';
+import { DefaultComponent } from './admin/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: DefaultComponent,
+  children : [
+    {path: 'dashboard', component: DashboardComponent},
+    {path:'posts', component: PostsComponent}
+    
+
+   
+  ]
+
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
